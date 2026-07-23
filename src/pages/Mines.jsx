9c4916a1cd19af90691cdd15fe332f3bb63dec11
Armaan Sharma <ap@Armaans-MacBook-Air.local>
2026-07-23 22:31:49 +0530

    Implement random mine generation

--- src/pages/Mines.jsx
+++ src/pages/Mines.jsx
@@ -1,31 +1,46 @@
 import { useState } from "react";
 
 import MineGrid from "../components/mines/MineGrid";
+
 import createBoard from "../utils/createBoard";
+import placeMines from "../utils/placeMines";
 
 const Mines = () => {
   const [board, setBoard] = useState(createBoard());
 
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const startGame = () => {
+    const newBoard = placeMines(board, 5);
+
+    setBoard(newBoard);
+
+    setGameStarted(true);
+
+    console.log(newBoard);
+  };
+
   const handleTileClick = (id) => {
-    setBoard((prev) =>
-      prev.map((tile) =>
-        tile.id === id
-          ? {
-              ...tile,
-              revealed: true,
-              type: "gem",
-            }
-          : tile
-      )
-    );
+    if (!gameStarted) return;
+
+    console.log(id);
   };
 
   return (
-    <div className="min-h-screen bg-[var(--bg)] flex items-center justify-center">
+    <div className="min-h-screen bg-[var(--bg)] flex flex-col items-center justify-center gap-8">
+
+      <button
+        onClick={startGame}
+        className="bg-blue-500 px-8 py-3 rounded-xl font-bold"
+      >
+        Bet
+      </button>
+
       <MineGrid
         board={board}
         onTileClick={handleTileClick}
       />
+
     </div>
   );
 };
--- src/utils/createBoard.js
+++ src/utils/createBoard.js
@@ -1,6 +1,7 @@
 const createBoard = () => {
   return Array.from({ length: 25 }, (_, index) => ({
     id: index,
+    mine: false,
     revealed: false,
     type: "hidden",
   }));
